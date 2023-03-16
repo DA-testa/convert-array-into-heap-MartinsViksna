49acd78a0f59c262c71345cf6bf5b26449ec18cd
MartinsViksna <125703411+MartinsViksna@users.noreply.github.com>
2023-03-16 15:29:22 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -3,6 +3,26 @@
 
 def build_heap(data):
     swaps = []
+    gar = len(data)
+    for i in range(gar//2-1,-1,1):
+        root = i 
+        boo = true
+        while boo:
+            leftnode = 2*i+1
+            rightnode = 2*i+2
+            if  leftnode <= gar-1 and data[leftnode]<data[root]:
+                root = leftnode
+            else:
+                root = rightnode
+            if i<>root:
+                swaps.append(i,root)
+                data[i], data[root]=data[root],data[i]
+                i = root
+            else:
+                boo = false
+    return swaps
+
+            
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
 
@@ -11,15 +31,31 @@ def build_heap(data):
 
 
 def main():
-    
+    x = input()
+
+    if 'F' in x:
+        file_name = input()
+        file_name = "test/" + file_name
+        try:
+
+            with open(file_name)as file:
+                readl = int(file.readline())
+                parents = np.array( list(map(int, file.readline().split())))
+                print(compute_height(readl, parents))
+        except IOError:
+            print("no such file")
+            return
+
+    elif 'I' in x:
+        n = int(input())
+        data = list(map(int, input().split()))
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
 
 
     # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
